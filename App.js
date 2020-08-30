eeb319768c9b5a3cdf807ee8f2206da569437db6
genilton10 <geniltonlisboa@hotmail.com>
2020-08-30 00:08:06 -0300

    rn-02

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
+
 import React from 'react';
 import {Text} from 'react-native';
+import styled from 'styled-components';
 
+const Texto = styled.Text`
+  font-size: 30px
+  color: #00ffff;
+`;
+const Pagina = styled.SafeAreaView`
+  background-color: yellow;
+  flex: 1;
+`;
 export default () => {
   
-  return <Text> Utilizando text do react-native para fazer Ola Mundo!</Text>;
+  return (
+    <Pagina>
+      <Text>Ola Mundo I</Text>
+      <Text>Ola Mundo II</Text>
+      <Texto>Ola Mundo Novo</Texto>
+    </Pagina>
+  );
 };
