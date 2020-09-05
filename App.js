a007e5c066fd3097ae57520897ed6504cec23497
genilton10 <geniltonlisboa@hotmail.com>
2020-09-05 00:39:34 -0300

    rn-05

--- App.js
+++ App.js
@@ -1,37 +1,31 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import {Text, Button} from 'react-native';
 
 const Pagina = styled.SafeAreaView`
   flex: 1;
   justify-content: center;
-  flex-direction: row;
-  flex-wrap:  wrap;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const Quadrado = styled.View`
-  height: 100px;
-  width: 50px;
-  background-color: ${(props) => props.cor};
- 
-`;
-
-export default () => {
+const Paragrafo = (props) => {
+  return <Text>{props.frase}</Text>;
+};
   
+export default () => {
+  const [valor, alteraValor] = useState(4);
+
+  const incrementar = () =>{
+      alteraValor(valor+1);
+  };  
   return (
     <Pagina>
-      <Quadrado cor="red" />
-      <Quadrado cor="yellow" />
-      <Quadrado cor="green" />
-      <Quadrado cor="blue" />
-      <Quadrado cor="red" />
-      <Quadrado cor="yellow" />
-      <Quadrado cor="green" />
-      <Quadrado cor="blue" />
-      <Quadrado cor="red" />
-      <Quadrado cor="yellow" />
-      <Quadrado cor="green" />
-      <Quadrado cor="blue" />
+      <Text>{valor}</Text>
+      <Text>{valor}</Text>
+      <Button title="Incrementar" onPress={incrementar} />
+      <Paragrafo frase="Frase passada por props"/>
+      <Paragrafo frase="nova frase"/>
     </Pagina>
   );
 };
